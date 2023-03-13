a3f842836fd9a4e9dff1feffc085d91da72dec37
caioba <chiqueto.caio@gmail.com>
2023-03-13 20:32:20 -0300

    adicionando a segunda tela somente

--- duasTelas/app/src/main/java/com/example/duastelas/MainActivity.kt
+++ duasTelas/app/src/main/java/com/example/duastelas/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.duastelas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnGoTotela2: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnGoTotela2 = findViewById(R.id.btnGoToTela1)
+        btnGoTotela2.setOnClickListener {
+            val intent = Intent(this, tela2::class.java)
+            startActivity(intent)
+        }
     }
 }
