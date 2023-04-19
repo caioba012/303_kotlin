3a7b0a5fa1f3e5baf8b72b83be6618e0679c0c13
caioba <chiqueto.caio@gmail.com>
2023-04-19 20:37:05 -0300

    Aula 19/04/2023

--- LendoGravando/app/src/main/java/com/example/lendogravando/MainActivity.kt
+++ LendoGravando/app/src/main/java/com/example/lendogravando/MainActivity.kt
@@ -1,9 +1,20 @@
 package com.example.lendogravando
 
+import android.Manifest.permission.CAMERA
+import android.app.Activity
+import android.content.ContentValues
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import com.example.lendogravando.databinding.ActivityMainBinding
 import java.io.BufferedInputStream
 import java.io.BufferedReader
@@ -20,6 +31,21 @@ class MainActivity : AppCompatActivity() {
     var fileName = "DS304_${System.currentTimeMillis()}.txt"
     internal var extFile: File?=null
     private val filepath = "Minha_foto"
+
+    private var imageUri: Uri? = null
+
+    //chamada simples de requisição de permissão
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ){  isGranted: Boolean ->
+            if (isGranted){
+                Log.i("Permissions","Aceita")
+                binding.btnFoto.isEnabled = true
+            }else {
+                Log.i("Permissions", "Negada")
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -63,7 +89,46 @@ class MainActivity : AppCompatActivity() {
             binding.btnGrava.isEnabled = true
         }
 
+        //Trataremos o botao para tirar a foto
+        if(ContextCompat.checkSelfPermission(this, CAMERA)!=
+                PackageManager.PERMISSION_GRANTED) {
+            requestPermissionLauncher.launch(CAMERA)
+        }else{
+            binding.btnFoto.isEnabled = true
+        }
+
+        binding.btnFoto.setOnClickListener {
+            imageUri = initFileUri()
+            val values = ContentValues()
+            values.put(MediaStore.Images.Media.TITLE,"Foto Tirada")
+            values.put(MediaStore.Images.Media.DESCRIPTION, "Aula 303")
+
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            imageUri = this?.contentResolver?.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+            launcher.launch(intent)
+        }
     }
+
+    val launcher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ){
+        if(it.resultCode == Activity.RESULT_OK){
+            binding.imageView?.setImageURI(imageUri)
+        }
+    }
+
+    private fun initFileUri():Uri{
+        val filepath = "fotos"
+        val fileName = "Foto_${System.currentTimeMillis()}.jpg"
+        //cria o arquivo no diretorio acima
+        val jpgImage = File(getExternalFilesDir(filepath),fileName)
+
+        return FileProvider.getUriForFile(applicationContext,
+            "com.example.fileprovider2",
+            jpgImage)
+    }
+
 
     private val isExternalStorageAvailable: Boolean get(){
         val extStorageState =  Environment.getExternalStorageState()
